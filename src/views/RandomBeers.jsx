4e492dc68b7c8802504ed8dd1f0468b742f9bf79
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-12 17:48:09 +0100

    'Iteration 5 | A random beer'

--- src/views/RandomBeers.jsx
+++ src/views/RandomBeers.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
+import axios from "axios";
 
 export default function RandomBeers() {
+  const [beerRandom, setBeerRandom] = useState(null);
+
+  const getBeer = async () => {
+    try {
+      const response = await axios.get(
+        `https://ih-beers-api2.herokuapp.com/beers/random`
+      );
+      console.log(response);
+      setBeerRandom(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBeer();
+  }, []);
+
   return (
-    <div>
-      <div>
-        <Header />
-        <h1>RandomBeers</h1>
-      </div>
+    <div className="page">
+      <Header />
+      <h1>Beer Details</h1>
+      {beerRandom && (
+        <div className="detail-card">
+          <img src={beerRandom.image_url} alt={beerRandom.name} />
+          <div className="content">
+            <h2>{beerRandom.name}</h2>
+            <h4>{beerRandom.tagline}</h4>
+            <p>{beerRandom.attenuation_level}</p>
+            <p>{beerRandom.first_brewed}</p>
+            <p>{beerRandom.description}</p>
+            <p>{beerRandom.contributed_by}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
