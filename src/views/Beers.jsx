645b035db2db9a11b84c94084521ca60411f0e58
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-12 13:06:03 +0100

    'Iteration 3 | List all the beers'

--- src/views/Beers.jsx
+++ src/views/Beers.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
-
-
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Beers() {
+  const [beers, setBeers] = useState([]);
+
+  const getBeers = async () => {
+    try {
+      const response = await axios.get(
+        "https://ih-beers-api2.herokuapp.com/beers"
+      );
+      setBeers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBeers();
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <Header />
       <h1>All Beers</h1>
+
+      {beers.map((elem) => {
+        return (
+          <div key={elem.id}>
+            <img width="150px" src={elem.image_url} alt={elem.name} />
+            <h2>{elem.name}</h2>
+            <p>{elem.tagline}</p>
+            <p>{elem.contributed_by}</p>
+            <Link to={`/beers/${elem.id}`}>See more</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
